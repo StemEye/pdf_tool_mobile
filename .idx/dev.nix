17aa3907b7fe300fa5d66be999fae3463ee5575f
babar.bashir92 <babar.bashir92@gmail.com>
2024-09-18 11:56:49 +0000

    removed comments

---
+++ .idx/dev.nix
@@ -0,0 +1,42 @@
+{pkgs}: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodePackages.firebase-tools
+    pkgs.jdk17
+    pkgs.unzip
+    pkgs.adoptopenjdk-jre-openj9-bin-11
+  ];
+  idx.extensions = [
+    
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "flutter"
+          "run"
+          "--machine"
+          "-d"
+          "web-server"
+          "--web-hostname"
+          "0.0.0.0"
+          "--web-port"
+          "$PORT"
+        ];
+        manager = "flutter";
+      };
+      android = {
+        command = [
+          "flutter"
+          "run"
+          "--machine"
+          "-d"
+          "android"
+          "-d"
+          "emulator-5554"
+        ];
+        manager = "flutter";
+      };
+    };
+  };
+}
